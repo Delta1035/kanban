9f7905c1ee50fe99939428f1ffe25900ce80fc23
delta <528491526@qq.com>
2022-08-08 01:10:21 +0800

    feat: 增加防抖hook

--- src/utils/hooks/useDebounce.ts
+++ src/utils/hooks/useDebounce.ts
@@ -1,1 +1,12 @@
-export const useDebounce = (value: any, delay: number) => {};
+import { useEffect, useState } from "react";
+
+export const useDebounce = (value: any, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+  return debouncedValue;
+};
